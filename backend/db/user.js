cb01ac823414387aaa0fdba525cb2f8e1de5b58d
Sebastian <scorcino@cincinnatus.edu.do>
2022-05-26 10:33:10 -0400

    Modelo User

---
+++ backend/db/user.js
@@ -0,0 +1,32 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("sqlite::memory:");
+
+const User = sequelize.define(
+  "User",
+  {
+    // Model attributes are defined here
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    // Other model options go here
+  }
+);
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
